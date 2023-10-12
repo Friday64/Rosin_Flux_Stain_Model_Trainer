40a2325ca8352d9673f3913ff41911951660adb2
Friday64 <mcfrantz12@gmail.com>
2023-10-11 20:50:35 -0400

    Update Flux_Stain_Trainer_AI.py
    Added Changes to train_model function and added threading

--- Flux_Stain_Trainer_AI.py
+++ Flux_Stain_Trainer_AI.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import filedialog
 import threading
 import logging
 import os
@@ -12,7 +12,6 @@ from keras.regularizers import l2
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from PIL import Image
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
@@ -26,9 +25,10 @@ with_flux_folder = None
 without_flux_folder = None
 output_folder = None
 
-# Initialize a variable to control training
+# Global variable for stopping the training
 stop_training = False
 
+# Function to stop training
 def stop_training_func():
     global stop_training
     stop_training = True
@@ -36,15 +36,17 @@ def stop_training_func():
 
 # Function to select folder and update status label
 def select_folder(folder_type, status_label):
-    global with_flux_folder, without_flux_folder, output_folder
     folder_selected = filedialog.askdirectory()
     if folder_selected:
         status_label.config(text="Selected", fg="green")
         if folder_type == 'With Flux':
+            global with_flux_folder
             with_flux_folder = folder_selected
         elif folder_type == 'Without Flux':
+            global without_flux_folder
             without_flux_folder = folder_selected
         elif folder_type == 'Model Output':
+            global output_folder
             output_folder = folder_selected
 
 # Function to load dataset from folder
@@ -67,14 +69,12 @@ def load_dataset(folder_path, label):
 def plot_data(accuracy, loss):
     fig, (ax1, ax2) = plt.subplots(1, 2)
     ax1.plot(accuracy)
-    ax1.set_title('Model Accuracy (%)')  # Display as %
-    ax1.set(xlabel='epoch', ylabel='accuracy (%)')
+    ax1.set_title('Model Accuracy (%)')
+    ax1.set(xlabel='epoch', ylabel='accuracy')
     ax2.plot(loss)
     ax2.set_title('Model Loss')
     ax2.set(xlabel='epoch', ylabel='loss')
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().pack()
+    plt.show()
 
 # Function to train machine learning model
 def train_model():
@@ -95,8 +95,8 @@ def train_model():
     base_model = ResNet50(weights='imagenet', include_top=False)
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dropout(0.5)(x)  # Added Dropout layer
-    predictions = Dense(1, activation='sigmoid', kernel_regularizer=l2(0.01))(x)  # Added L2 regularization
+    x = Dropout(0.5)(x)
+    predictions = Dense(1, activation='sigmoid', kernel_regularizer=l2(0.01))(x)
     model = Model(inputs=base_model.input, outputs=predictions)
     
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -111,33 +111,27 @@ def train_model():
     
     model.save(f"{output_folder}/my_model.h5")
     
-    plot_data(history.history['accuracy'], history.history['loss'])
+    plot_data([round(acc * 100, 2) for acc in history.history['accuracy']], history.history['loss'])
 
-with_flux_status_label = tk.Label(root, text="Not Selected", fg="red")
-with_flux_status_label.pack()
+# GUI Components
+frame = tk.Frame(root)
+frame.pack(pady=20)
 
-select_with_flux_folder_button = tk.Button(root, text="Select With Flux Folder",
-                                           command=lambda: select_folder('With Flux', with_flux_status_label))
-select_with_flux_folder_button.pack()
+# Folder selection buttons and status labels
+for folder_type in ['With Flux', 'Without Flux', 'Model Output']:
+    folder_frame = tk.Frame(frame)
+    folder_frame.pack(side="left", padx=10)
+    
+    status_label = tk.Label(folder_frame, text="Not Selected", fg="red")
+    status_label.pack(side="bottom")
+    
+    button = tk.Button(folder_frame, text=f"Select {folder_type} Folder",
+                       command=lambda f=folder_type, s=status_label: select_folder(f, s))
+    button.pack()
 
-without_flux_status_label = tk.Label(root, text="Not Selected", fg="red")
-without_flux_status_label.pack()
-
-select_without_flux_folder_button = tk.Button(root, text="Select Without Flux Folder",
-                                              command=lambda: select_folder('Without Flux', without_flux_status_label))
-select_without_flux_folder_button.pack()
-
-output_folder_status_label = tk.Label(root, text="Not Selected", fg="red")
-output_folder_status_label.pack()
-
-select_output_folder_button = tk.Button(root, text="Select Model Output Folder",
-                                        command=lambda: select_folder('Model Output', output_folder_status_label))
-select_output_folder_button.pack()
-
-start_button = tk.Button(root, text="Start Training", command=train_model)
+start_button = tk.Button(root, text="Start Training", command=lambda: threading.Thread(target=train_model).start())
 start_button.pack()
 
-# Added Stop button
 stop_button = tk.Button(root, text="Stop Training", command=stop_training_func)
 stop_button.pack()
 
