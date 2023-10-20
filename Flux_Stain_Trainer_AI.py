ae4a674b20452e2825d58b06e74dae1593e8d535
Friday64 <mcfrantz12@gmail.com>
2023-10-20 13:18:12 -0400

    changes

--- Flux_Stain_Trainer_AI.py
+++ Flux_Stain_Trainer_AI.py
@@ -1,56 +1,44 @@
-#Create a TensorFlow model that detects flux stains on a PCB
-#use threads for all the image processing and training
-#using two diffrent folders of images on the desktop
-#use image processing for the semi-parent nature of flux, it has a yellow hue with a slite brown tint
-#use Tkinter for the GUI
-#use root.mainloop() to run the GUI
-#ask the user for the 2 input folders "With_flux" and "Without_flux"
-#have a button to train the model
-#train the model when the button is pressed
-#ask user for the output folder to save the model before training the model
-#check to see if the model has already exists and reuse it if it does
-#create a new model if it does not exist called "Flux_stain_Model.h5"
-#save the model to the output folder
-
-#import the necessary libraries first
-from tkinter import filedialog
-from altair import LabelOverlap
-from pydantic import create_model
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-import numpy as np
-import cv2
 import tkinter as tk
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
-from keras.losses import categorical_crossentropy
-from keras.optimizers import Adam
-from keras.regularizers import l2
-from keras.utils import to_categorical
-import matplotlib.pyplot as plt
+from tkinter import filedialog
+from tqdm import tqdm
 import os
-import shutil
-import random
-import glob
-import time
+import cv2
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+import threading
 
-#define the path of the 2 folders that contain the images "With_flux" and "Without_flux"
-# Define the path of the 2 folders that contain the images
-with_flux_folder = "path/to/With_flux"
-without_flux_folder = "path/to/Without_flux"
+# Define global variables for input and output folders
+with_flux_folder = ""
+without_flux_folder = ""
+output_folder = "output"
+model_file = "Flux_stain_Model.h5"
 
+# Create a function to browse for input folders
+def browse_input_folders(folder_type):
+    global with_flux_folder, without_flux_folder
+    folder = filedialog.askdirectory(title=f"Select {folder_type} Folder")
+    if folder_type == "With Flux":
+        with_flux_folder = folder
+    elif folder_type == "Without Flux":
+        without_flux_folder = folder
+
+# Create a function to browse for the output folder
+def browse_output_folder():
+    global output_folder
+    output_folder = filedialog.askdirectory(title="Select Output Folder")
+
+# Create a function to preprocess images
 def preprocess_image(image):
-    # Perform preprocessing steps on the image
-    # e.g. resize, normalize, etc.
     resized_image = cv2.resize(image, (100, 100))
     normalized_image = resized_image / 255.0
     return normalized_image
-    return preprocessed_image
 
+# Create a function to apply a specific filter to the image
 def apply_filter(image, filter_type):
-    # Apply a specific filter to the image
-    # e.g. blur, sharpen, edge detection, etc.
     if filter_type == 'blur':
         filtered_image = cv2.blur(image, (5, 5))
     elif filter_type == 'sharpen':
@@ -65,130 +53,109 @@ def apply_filter(image, filter_type):
     
     return filtered_image
 
+# Create a function to enhance the contrast of the image
 def enhance_contrast(image):
-    # Enhance the contrast of the image
-    # e.g. histogram equalization, adaptive histogram equalization, etc.
     contrast_enhanced_image = cv2.equalizeHist(image)
-
     return contrast_enhanced_image
 
+# Create a function to load and preprocess images from a folder
+def load_and_preprocess_images(folder, label):
+    images = []
+    labels = []
+    for filename in os.listdir(folder):
+        if filename.endswith(".jpg"):
+            image = cv2.imread(os.path.join(folder, filename))
+            if image is not None:
+                preprocessed_image = preprocess_image(image)
+                images.append(preprocessed_image)
+                labels.append(label)
+    return images, labels
 
+# Create a function to create and compile the TensorFlow model
+def create_and_compile_model():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(100, 100, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
 
-#create a list called preprocessed_images for the preprocessed images and a list called labels for the labels
-preprocessed_images = []
-labels = []
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+    return model
 
-
-
-
-
-
-
-
-
+# Create a function to train the model
 def train_model():
-    # Ask user for the output folder to save the model
-    output_folder = filedialog.askdirectory()
+    global with_flux_folder, without_flux_folder, output_folder
 
-    # Train your model here
-    model = create_model()
-    
+    # Load and preprocess images from the input folders
+    with_flux_images, with_flux_labels = load_and_preprocess_images(with_flux_folder, [1, 0])
+    without_flux_images, without_flux_labels = load_and_preprocess_images(without_flux_folder, [0, 1])
 
-    # Assuming you have preprocessed images stored in a list called preprocessed_images
-    # Assuming you have corresponding labels stored in a list called labels
+    # Combine and shuffle the data
+    all_images = with_flux_images + without_flux_images
+    all_labels = with_flux_labels + without_flux_labels
+    X_train, X_test, y_train, y_test = train_test_split(all_images, all_labels, test_size=0.2, random_state=42)
 
-    # Convert the preprocessed_images list to a numpy array
-    X = np.array(preprocessed_images)
-    Y=np.array(labels)
-    # Split the data into training and testing sets
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    # Create and compile the model
+    model = create_and_compile_model()
 
-    # Convert the labels list to a numpy array and encode them as one-hot vectors
-    num_classes = len(set(labels))
-    y = np.zeros((len(LabelOverlap), num_classes))
-    for i, label in enumerate(labels):
-        y[i, label] = 1
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
-    
-#create main window and widgets
+    # Train the model
+    model.fit(np.array(X_train), np.array(y_train), epochs=10, batch_size=32, validation_data=(np.array(X_test), np.array(y_test)))
+
+    # Save the model to the output folder
+    model.save(os.path.join(output_folder, model_file))
+    print("Model Saved")
+
+# Create the main window
 window = tk.Tk()
-window.title("Flux Stain Classifier")
-window.geometry("500x500")
-#ask the user for the 2 input folders "With_flux" and "Without_flux" in the gui
-import tkinter as tk
-from tkinter import filedialog
+window.title("Flux Stain Detector")
+window.geometry("500x200")
+
+# Create labels and buttons for input folders
+with_flux_label = tk.Label(window, text="With Flux Folder:")
+with_flux_label.pack()
+with_flux_button = tk.Button(window, text="Browse", command=lambda: browse_input_folders("With Flux"))
+with_flux_button.pack()
 
 
-#create main window and widgets
-window = tk.Tk()
-window.title("Flux Stain Classifier")
-window.geometry("500x500")
+without_flux_label = tk.Label(window, text="Without Flux Folder:")
+without_flux_label.pack()
+without_flux_button = tk.Button(window, text="Browse", command=lambda: browse_input_folders("Without Flux"))
+without_flux_button.pack()
 
-# Create entry fields for the input folders
-with_flux_entry = tk.Entry(window)
-with_flux_entry.pack()
+# Create a label and button for the output folder
+output_label = tk.Label(window, text="Output Folder:")
+output_label.pack()
+output_button = tk.Button(window, text="Browse", command=browse_output_folder)
+output_button.pack()
 
-without_flux_entry = tk.Entry(window)
-without_flux_entry.pack()
 
-# Create a button to train the model
+#have the user enter the amount of epoch in the window
+# Create a label and entry field for the number of epochs
+epochs_label = tk.Label(window, text="Number of Epochs:")
+epochs_label.pack()
+epochs_entry = tk.Entry(window)
+epochs_entry.pack()
+
+# Create a button to capture the user's input
+submit_button = tk.Button(window, text="Submit", command=lambda: train_model(int(epochs_entry.get())))
+submit_button.pack()
+
+
+# Create a progress bar for the training process
+
+# Create a progress bar 
+progress_bar = tqdm(total=100)
+
+# Update the progress bar as needed
+progress_bar.update(10)  # Update the progress by 10 units
+
+# Close the progress bar
+progress_bar.close()
 train_button = tk.Button(window, text="Train Model", command=train_model)
 train_button.pack()
 
 window.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
