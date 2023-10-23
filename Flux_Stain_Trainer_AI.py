a7078f18f574df1d357de81cc9aff1ec6a3bef93
Friday64 <mcfrantz12@gmail.com>
2023-10-22 21:03:41 -0400

    Major changes

--- Flux_Stain_Trainer_AI.py
+++ Flux_Stain_Trainer_AI.py
@@ -1,4 +1,3 @@
-# Simplified Imports
 import os
 import cv2
 import numpy as np
@@ -10,6 +9,7 @@ from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.callbacks import EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import to_categorical
+
 
 # Global Variables
 with_flux_folder = ""
@@ -31,83 +31,132 @@ def browse_output(label_widget):
     output_folder = filedialog.askdirectory(title="Select Output Folder")
     if output_folder:
         label_widget.config(text=output_folder)
-
 def preprocess_and_load_images(folder, label):
-    return [(cv2.resize(cv2.imread(os.path.join(folder, f)), (100, 100)) / 255.0, label) 
-            for f in os.listdir(folder) if f.endswith(".jpg")]
+    """
+    Loads images from the folder, preprocesses them, and returns as an array.
+    
+    Parameters:
+        folder (str): The folder path where the images are stored.
+        label (int): The label to be used for these images.
+        
+    Returns:
+        list: A list of tuples where the first element is the image array and the second is the label.
+    """
+    # Validate the folder exists
+    if not os.path.exists(folder):
+        print(f"Folder {folder} does not exist.")
+        return []
+
+    processed_images = []
+    
+    # Loop through all jpg files in the folder
+    for filename in os.listdir(folder):
+        if filename.endswith(".jpg"):
+            # Read and resize the image
+            img = cv2.imread(os.path.join(folder, filename))
+            img_resized = cv2.resize(img, (100, 100))
+            
+            # Normalize pixel values
+            img_normalized = img_resized / 255.0
+            
+            processed_images.append((img_normalized, label))
+            
+    return processed_images
 
 def create_model():
-    model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
-        MaxPooling2D(2, 2),
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2),
-        Flatten(),
-        Dense(128, activation='relu'),
-        Dropout(0.5),
-        Dense(2, activation='softmax')
-    ])
+    """
+    Creates a Convolutional Neural Network (CNN) model for classifying images with or without flux stains.
+
+    Returns:
+        keras.models.Sequential: The CNN model
+    """
+    # Initialize the model
+    model = Sequential()
+
+    # Add layers
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    
+    # Output layer
+    model.add(Dense(2, activation='softmax'))
+
+    # Compile the model
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
     return model
 
-from tensorflow.keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.models import load_model
-import numpy as np
-import os
 
 def train_model(epochs):
-    # Load or create the model
-    model_path = 'Flux_Stain_Model.h5'
-    model = load_model(model_path) if os.path.exists(model_path) else create_model()
-    
-    # Load and preprocess images
+    # Prepare the image data
     with_flux_data = preprocess_and_load_images(with_flux_folder, 1)
     without_flux_data = preprocess_and_load_images(without_flux_folder, 0)
     
-    # Combine and split the dataset
-    all_images, all_labels = zip(*(with_flux_data + without_flux_data))
+    # Combine both types of data
+    all_data = with_flux_data + without_flux_data
+    all_images, all_labels = zip(*all_data)
+
+    # Convert to NumPy arrays
+    all_images = np.array(all_images)
+    all_labels = np.array(all_labels)
     
-    # Always good to check shapes for debugging
-    print(f"Shape of all_images: {np.array(all_images).shape}")
-    print(f"Shape of all_labels: {np.array(all_labels).shape}")
-    
+    # Split the dataset
     X_train, X_test, y_train, y_test = train_test_split(all_images, all_labels, test_size=0.2)
-    
+
     # Convert labels to categorical
     y_train, y_test = to_categorical(y_train, 2), to_categorical(y_test, 2)
     
+    # Create or load the model
+    model_path = 'Flux_Stain_Model.h5'
+    model = load_model(model_path) if os.path.exists(model_path) else create_model()
+
     # Data augmentation
     datagen = ImageDataGenerator(rotation_range=20, width_shift_range=0.2,
                                  height_shift_range=0.2, horizontal_flip=True)
     
     # Fit the data generator
-    datagen.fit(np.array(X_train))  # Removed the extra dimension addition
+    datagen.fit(X_train)
     
-    # Early stopping
-    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+    # Early stopping callback
+    early_stopping = EarlyStopping(monitor='val_loss', patience=50)
     
-    # Training the model
-    model.fit(datagen.flow(np.array(X_train), np.array(y_train), batch_size=32),
-              epochs=epochs, validation_data=(np.array(X_test), np.array(y_test)),
+    # Train the model
+    model.fit(datagen.flow(X_train, y_train, batch_size=32),
+              epochs=epochs,
+              validation_data=(X_test, y_test),
               callbacks=[early_stopping])
     
-    # Save the model
+    # Save the trained model
     model.save(os.path.join(output_folder, model_path))
-    print("Model Saved")
-
-# Remember to type this code manually for better retention!
+    print("Model training complete and saved!")
 
 def start_training():
-    num_epochs = int(epochs_entry.get())
-    train_model(num_epochs)
+    try:
+        # Read the number of epochs from the Entry widget
+        num_epochs = int(epochs_entry.get())
+        
+        # Validate the number of epochs
+        if num_epochs < 1:
+            print("Please enter a valid number of epochs.")
+            return
+
+        # Call the function to train the model
+        train_model(num_epochs)
+        print("Training started with {} epochs.".format(num_epochs))
+    except ValueError:
+        print("Please enter a valid number for epochs.")
 
 # Tkinter GUI
 window = tk.Tk()
 window.title("Flux Stain Detector")
 
+# Folders for images with flux and without flux
 folders = [("With Flux", 'with_flux_display'), ("Without Flux", 'without_flux_display')]
 
 for folder_type, label_name in folders:
@@ -116,14 +165,18 @@ for folder_type, label_name in folders:
     label_name.pack()
     tk.Button(window, text="Browse", command=lambda l=label_name, f=folder_type: browse_folder(l, f)).pack()
 
+# Output Folder
 output_display = tk.Label(window, text="Not Selected")
 output_display.pack()
 tk.Label(window, text="Output Folder:").pack()
 tk.Button(window, text="Browse", command=lambda: browse_output(output_display)).pack()
 
+# Number of Epochs
 epochs_entry = tk.Entry(window)
 epochs_entry.pack()
 tk.Label(window, text="Number of Epochs:").pack()
+
+# Button to Start Training
 tk.Button(window, text="Start Training", command=start_training).pack()
 
 window.mainloop()
