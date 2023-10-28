e7f3680abb3fb97e5343925a78e49b368258a1d7
Friday64 <mcfrantz12@gmail.com>
2023-10-28 12:02:59 -0400

    changes

--- Flux_Stain_Trainer_AI.py
+++ Flux_Stain_Trainer_AI.py
@@ -1,28 +1,22 @@
 import os
 import cv2
 import numpy as np
-import tensorflow as tf
-import threading
+import tkinter as tk
+from tkinter import filedialog
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential, load_model
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from keras.callbacks import EarlyStopping, Callback
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import to_categorical
-from tkinter import Tk, Label, Button, filedialog, Entry
+from tensorflow.lite import TFLiteConverter
+import threading
 
 # Global Variables
 with_flux_folder = ""
 without_flux_folder = ""
 output_folder = "output"
 stop_training = False
-
-# Custom Callback to Stop Training
-class StopTrainingCallback(Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        global stop_training
-        if stop_training:
-            self.model.stop_training = True
 
 def browse_folder(label_widget, folder_type):
     folder = filedialog.askdirectory(title=f"Select {folder_type} Folder")
@@ -56,9 +50,9 @@ def preprocess_and_load_images(folder, label):
 def create_model():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)))
-    model.add(MaxPooling2D((2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
@@ -66,68 +60,80 @@ def create_model():
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-def convert_to_tflite(model_path):
-    model = load_model(model_path)
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+def convert_to_tflite(model):
+    converter = TFLiteConverter.from_keras_model(model)
     tflite_model = converter.convert()
     with open(os.path.join(output_folder, "Flux_Stain_Model.tflite"), "wb") as f:
         f.write(tflite_model)
+    print("Model converted to TFLite format.")
 
 def train_model(epochs):
     global stop_training
-    stop_training = False  # Reset stop_training flag
+    stop_training = False
     
-    with_flux_data = preprocess_and_load_images(with_flux_folder, 1)
-    without_flux_data = preprocess_and_load_images(without_flux_folder, 0)
-    all_data = with_flux_data + without_flux_data
-    all_images, all_labels = zip(*all_data)
-    all_images = np.array(all_images)
-    all_labels = np.array(all_labels)
-    X_train, X_test, y_train, y_test = train_test_split(all_images, all_labels, test_size=0.2)
-    y_train, y_test = to_categorical(y_train, 2), to_categorical(y_test, 2)
-    model_path = os.path.join(output_folder, "Flux_Stain_Model")
+    model_path = os.path.join(output_folder, 'Flux_Stain_Model.h5')
     model = load_model(model_path) if os.path.exists(model_path) else create_model()
-    datagen = ImageDataGenerator(rotation_range=20, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True)
-    datagen.fit(X_train)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=50)
-    stop_callback = StopTrainingCallback()
+
+    with_flux_data = preprocess_and_load_images(with_flux_folder, 0)
+    without_flux_data = preprocess_and_load_images(without_flux_folder, 1)
+    all_data = with_flux_data + without_flux_data
+
+    X = np.array([i[0] for i in all_data])
+    y = to_categorical([i[1] for i in all_data])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     
-    model.fit(datagen.flow(X_train, y_train, batch_size=32), epochs=epochs, validation_data=(X_test, y_test), callbacks=[early_stopping, stop_callback])
+    early_stop = EarlyStopping(monitor='val_loss', patience=3, verbose=1)
+    model.fit(X_train, y_train, epochs=epochs, validation_data=(X_test, y_test), callbacks=[early_stop])
+    
     model.save(model_path)
-    convert_to_tflite(model_path)
+    if not stop_training:
+        convert_to_tflite(model)
+    
+    print("Model training complete and saved!")
 
-def start_training_threaded():
-    num_epochs = int(epochs_entry.get())
-    if num_epochs < 1:
-        print("Please enter a valid number of epochs.")
-        return
-    training_thread = threading.Thread(target=train_model, args=(num_epochs,))
-    training_thread.start()
 
-def stop_training_func():
+    model.save(model_path)
+    if not stop_training:
+        convert_to_tflite(model)
+    print("Model training complete and saved!")
+
+def start_training_thread():
+    try:
+        num_epochs = int(epochs_entry.get())
+        if num_epochs < 1:
+            print("Please enter a valid number of epochs.")
+            return
+        print(f"Training started with {num_epochs} epochs.")
+        threading.Thread(target=train_model, args=(num_epochs,)).start()
+    except ValueError:
+        print("Please enter a valid number for epochs.")
+
+def stop_training():
     global stop_training
     stop_training = True
 
 # Tkinter GUI
-window = Tk()
+window = tk.Tk()
 window.title("Flux Stain Detector")
 
 folders = [("With Flux", 'with_flux_display'), ("Without Flux", 'without_flux_display')]
 for folder_type, label_name in folders:
-    Label(window, text=f"{folder_type} Folder:").pack()
-    label_name = Label(window, text="Not Selected")
+    tk.Label(window, text=f"{folder_type} Folder:").pack()
+    label_name = tk.Label(window, text="Not Selected")
     label_name.pack()
-    Button(window, text="Browse", command=lambda l=label_name, f=folder_type: browse_folder(l, f)).pack()
+    tk.Button(window, text="Browse", command=lambda l=label_name, f=folder_type: browse_folder(l, f)).pack()
 
-output_display = Label(window, text=f"Output Folder: {output_folder}")
+output_display = tk.Label(window, text="Not Selected")
 output_display.pack()
-Button(window, text="Browse Output", command=lambda l=output_display: browse_output(l)).pack()
+tk.Label(window, text="Output Folder:").pack()
+tk.Button(window, text="Browse", command=lambda: browse_output(output_display)).pack()
 
-epochs_entry = Entry(window)
+epochs_entry = tk.Entry(window)
 epochs_entry.pack()
-epochs_entry.insert(0, "Enter epochs")
+tk.Label(window, text="Number of Epochs:").pack()
 
-Button(window, text="Start Training", command=start_training_threaded).pack()
-Button(window, text="Stop Training", command=stop_training_func).pack()
+tk.Button(window, text="Start Training", command=start_training_thread).pack()
+tk.Button(window, text="Stop Training", command=stop_training).pack()
 
 window.mainloop()
