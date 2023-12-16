e4b5346cc6ea10d01a3a6c65a833674e4c3f4cf8
Friday64 <mcfrantz12@gmail.com>
2023-12-16 07:47:06 -0500

    Calculate the Correct Input Size

--- Flux_Stain_Trainer_AI.py
+++ Flux_Stain_Trainer_AI.py
@@ -33,14 +33,15 @@ class FluxNet(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.fc1 = nn.Linear(64 * 32 * 32, 128)
+        # Dynamically calculate the size or replace 65536 with the correct number
+        self.fc1 = nn.Linear(65536, 128)  # Adjust 65536 to the correct number
         self.fc2 = nn.Linear(128, 2)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)  # Dynamically calculate the correct size
+        x = x.view(x.size(0), -1)  # Flatten the output for the linear layer
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
