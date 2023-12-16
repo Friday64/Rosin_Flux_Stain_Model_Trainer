c72061e7b4edc9c1ea17278afee28fc7290ec659
Friday64 <mcfrantz12@gmail.com>
2023-12-16 08:10:39 -0500

    made changes to fix model training metrics issues

--- Flux_Stain_Trainer_AI.py
+++ Flux_Stain_Trainer_AI.py
@@ -10,9 +10,10 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import threading
 from torchvision import transforms
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-# Debugging function to print message with variable state
+# Debugging function
 def debug_print(message, variable=None):
     if variable is not None:
         print(f"DEBUG: {message}: {variable}")
@@ -20,67 +21,87 @@ def debug_print(message, variable=None):
         print(f"DEBUG: {message}")
 
 # Paths to image folders and model
-with_flux_folder = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Data/with_flux"
-without_flux_folder = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Data/without_flux"
-output_folder = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Models"
+with_flux_folder = "path/to/With_Flux"
+without_flux_folder = "path/to/Without_Flux"
+output_folder = "path/to/Flux_Models"
 
+# Load data paths and labels
+all_data = []
+all_labels = []
+
+
+# Load images with flux (label 1)
+for filename in os.listdir(with_flux_folder):
+    if filename.endswith(('.png', '.jpg', '.jpeg')):
+        all_data.append(os.path.join(with_flux_folder, filename))
+        all_labels.append(1)  # Label 1 for with flux
+
+# Load images without flux (label 0)
+for filename in os.listdir(without_flux_folder):
+    if filename.endswith(('.png', '.jpg', '.jpeg')):
+        all_data.append(os.path.join(without_flux_folder, filename))
+        all_labels.append(0)  # Label 0 for without flux
+
+# Splitting the data into training and validation sets
+from sklearn.model_selection import train_test_split
+train_data, val_data, train_labels, val_labels = train_test_split(all_data, all_labels, test_size=0.2)
 # Size to which images will be resized
 img_size = (256, 256)
 
-# Define your neural network class
+# Neural network class
 class FluxNet(nn.Module):
     def __init__(self):
         super(FluxNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.fc1 = nn.Linear(262144, 128)  # Adjusted to the correct number
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(64 * 64 * 64, 128)  # Adjusted size
         self.fc2 = nn.Linear(128, 2)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
+        x = self.dropout(x)
         x = x.view(x.size(0), -1)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
-# Data handling in PyTorch
+# Dataset class
 class FluxDataset(Dataset):
-    def __init__(self, directory, transform=None):
-        self.directory = directory
+    def __init__(self, data, labels, transform=None):
+        self.data = data
+        self.labels = labels
         self.transform = transform
-        self.images = [os.path.join(directory, f) for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
     def __len__(self):
-        return len(self.images)
+        return len(self.data)
 
-   
     def __getitem__(self, idx):
-        img_name = self.images[idx]
+        img_name = self.data[idx]
+        label = self.labels[idx]
         image = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
         image = cv2.resize(image, img_size)
-        image = image / 255.0  # Normalize the image
-        image = np.expand_dims(image, axis=0)  # Add channel dimension
-        image = torch.from_numpy(image).float()  # Convert to PyTorch tensor
-        label = 1 if self.directory == with_flux_folder else 0
+        image = image / 255.0
+        image = np.expand_dims(image, axis=0)
+        image = torch.from_numpy(image).float()
         return image, label
 
-# Training function adapted for PyTorch with CPU usage
-def train_model_pytorch(epochs):
-    debug_print("Training model with epochs", epochs)
 
+# Create datasets and dataloaders
+train_dataset = FluxDataset(train_data, train_labels, transform=transforms.ToTensor())
+val_dataset = FluxDataset(val_data, val_labels, transform=transforms.ToTensor())
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+
+# Training function
+def train_model_pytorch(epochs):
     device = torch.device("cpu")
     model = FluxNet().to(device)
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()  # Adjust for class weights if necessary
     optimizer = optim.Adam(model.parameters(), lr=0.00001)
-
-    transform = transforms.Compose([transforms.ToTensor()])
-    train_dataset = FluxDataset(with_flux_folder, transform=transform)
-    test_dataset = FluxDataset(without_flux_folder, transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
     for epoch in range(epochs):
         model.train()
@@ -92,11 +113,9 @@ def train_model_pytorch(epochs):
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(images.float())
-
             _, predicted = torch.max(outputs.data, 1)
             all_labels.extend(labels.cpu().numpy())
             all_predictions.extend(predicted.cpu().numpy())
-
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -107,30 +126,24 @@ def train_model_pytorch(epochs):
         epoch_precision = precision_score(all_labels, all_predictions, average='binary')
         epoch_recall = recall_score(all_labels, all_predictions, average='binary')
         epoch_f1 = f1_score(all_labels, all_predictions, average='binary')
-
         debug_print(f"Epoch {epoch+1}, Loss: {epoch_loss}, Accuracy: {epoch_accuracy}, Precision: {epoch_precision}, Recall: {epoch_recall}, F1 Score: {epoch_f1}")
 
     torch.save(model.state_dict(), f"{output_folder}/flux_model.pth")
     debug_print("Training complete, model saved at", f"{output_folder}/flux_model.pth")
     messagebox.showinfo("Training Complete", "Model trained and saved successfully.")
 
-# Function to start training in a separate thread
+# Training start function
 def start_training():
     epochs = int(epochs_entry.get())
     debug_print("start_training called, starting thread")
     threading.Thread(target=train_model_pytorch, args=(epochs,)).start()
 
-# Initialize Tkinter window
+# Tkinter UI setup
 window = tk.Tk()
 window.title("Flux Stain Detector")
-
-# UI elements
 tk.Label(window, text="Number of Epochs:").pack()
 epochs_entry = tk.Entry(window)
 epochs_entry.pack()
-
 train_button = tk.Button(window, text="Train Model", command=start_training)
 train_button.pack()
-
-# Run the Tkinter event loop
 window.mainloop()
