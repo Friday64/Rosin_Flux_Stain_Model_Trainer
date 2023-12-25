7e9bb798c65d1a1af780cbfaa6ba47ba23c42d61
Friday64 <mcfrantz12@gmail.com>
2023-12-25 09:19:58 -0500

    merged program into one file

--- flux_stain_trainer.py
+++ flux_stain_trainer.py
@@ -1,4 +1,4 @@
-import argparse
+# Import necessary libraries
 import os
 import cv2
 import numpy as np
@@ -9,39 +9,42 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import tkinter as tk
 from tkinter import messagebox
+import threading  # For running training in a separate thread
 
-# Define your dataset paths and output folder
+# Define paths for the dataset and where the model will be saved
 with_flux_folder = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Data/With_Flux"
 without_flux_folder = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Data/Without_Flux"
 output_folder = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Models"
 
-# Load data paths and labels
+# Load data paths and labels for both categories: with and without flux
 all_data = []
 all_labels = []
 
-# Load images with flux (label 1)
+# Load images with flux and label them as 1
 for filename in os.listdir(with_flux_folder):
     if filename.endswith(('.png', '.jpg', '.jpeg')):
         all_data.append(os.path.join(with_flux_folder, filename))
         all_labels.append(1)
 
-# Load images without flux (label 0)
+# Load images without flux and label them as 0
 for filename in os.listdir(without_flux_folder):
     if filename.endswith(('.png', '.jpg', '.jpeg')):
         all_data.append(os.path.join(without_flux_folder, filename))
         all_labels.append(0)
 
-# Splitting the data into training and validation sets
+# Split the data into training and validation sets
 train_data, val_data, train_labels, val_labels = train_test_split(all_data, all_labels, test_size=0.2)
 
-# Size to which images will be resized
+# Define the size to which images will be resized
 img_size = (256, 256)
 
-# Neural network class
+# Neural network class definition for flux detection
 class FluxNet(nn.Module):
     def __init__(self):
         super(FluxNet, self).__init__()
+        # Define the layers and structure of the neural network
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
@@ -51,6 +54,7 @@ class FluxNet(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, x):
+        # Define the forward pass through the network
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
         x = self.dropout(x)
@@ -59,7 +63,7 @@ class FluxNet(nn.Module):
         x = self.fc2(x)
         return x
 
-# Dataset class
+# Custom Dataset class for loading and transforming images
 class FluxDataset(Dataset):
     def __init__(self, data, labels, transform=None):
         self.data = data
@@ -79,13 +83,13 @@ class FluxDataset(Dataset):
         image = torch.from_numpy(image).float()
         return image, label
 
-# Create datasets and dataloaders
+# Creating datasets and dataloaders for training and validation
 train_dataset = FluxDataset(train_data, train_labels, transform=transforms.ToTensor())
 val_dataset = FluxDataset(val_data, val_labels, transform=transforms.ToTensor())
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=1)
 val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=1)
 
-# Path to save or load the model
+# Specify the path to save or load the model
 model_path = f"{output_folder}/flux_model.pth"
 
 # Function to check for the model and train if not present
@@ -98,7 +102,7 @@ def check_and_train_model(model_path, train_loader, epochs):
         print("Model loaded successfully.")
     else:
         print("No pre-trained model found. Training a new model...")
-        model = train_model_pytorch(train_loader, model, epochs, device)  # Training the model
+        model = train_model_pytorch(train_loader, model, epochs, device)
         
     return model
 
@@ -136,15 +140,32 @@ def train_model_pytorch(train_loader, model, epochs, device):
     print("Training complete, model saved at", model_path)
     messagebox.showinfo("Training Complete", "Model trained and saved successfully.")
 
-# Command-line argument handling
-if __name__ == "__main__":
-    # Parse command-line arguments for epochs
-    parser = argparse.ArgumentParser(description='Train Flux Detector Model')
-    parser.add_argument('epochs', type=int, nargs='?', default=10, help='Number of epochs for training')  # Updated to handle optional epochs
-    args = parser.parse_args()
+# Function to handle threading for the training process
+def thread_training(epochs):
+    try:
+        epochs = int(epochs)
+    except ValueError:
+        messagebox.showerror("Error", "Please enter a valid integer for epochs.")
+        return
 
-    # Use the epochs from the command line or default if not provided
-    epochs = args.epochs
+    check_and_train_model(model_path, train_loader, epochs)
+    messagebox.showinfo("Training Complete", "Model trained and saved successfully.")
 
-    # Start the training process with the specified or default number of epochs
-    model = check_and_train_model(model_path, train_loader, epochs)
+# Function to start training (now using threading to avoid freezing the GUI)
+def start_training():
+    epochs = epochs_entry.get()
+    training_thread = threading.Thread(target=thread_training, args=(epochs,))
+    training_thread.start()
+
+# Tkinter GUI setup
+window = tk.Tk()
+window.title("Flux Stain Detector")
+
+tk.Label(window, text="Number of Epochs:").pack()
+epochs_entry = tk.Entry(window)
+epochs_entry.pack()
+
+train_button = tk.Button(window, text="Train Model", command=start_training)
+train_button.pack()
+
+window.mainloop()  # Start the Tkinter event loop
