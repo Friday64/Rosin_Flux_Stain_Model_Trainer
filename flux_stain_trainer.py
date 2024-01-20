fc1ef1a680cd34ba49b0ce9e8ad6438b26c30544
Friday64 <mcfrantz12@gmail.com>
2024-01-19 20:23:24 -0500

    Change

--- flux_stain_trainer.py
+++ flux_stain_trainer.py
@@ -18,7 +18,7 @@ logging.basicConfig(level=logging.INFO)
 # Constants for paths and hyperparameters
 WITH_FLUX_FOLDER = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Data/With_Flux"
 WITHOUT_FLUX_FOLDER = "C:/Users/Matthew/Desktop\Programming/Detect_Flux_Project/Flux_Data/Without_Flux"
-MODEL_PATH = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Model/flux_model_tf"
+MODEL_PATH = "C:/Users/Matthew/Desktop/Programming/Detect_Flux_Project/Flux_Models/flux_model_tf"
 IMG_SIZE = (256, 256)
 LEARNING_RATE = 0.0001
 BATCH_SIZE = 32
