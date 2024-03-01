f9c9e8f31d47cc057f160b11821407c105dd2dfa
Friday64 <mcfrantz12@gmail.com>
2024-02-29 22:14:25 -0500

    changes

---
+++ notes.md
@@ -0,0 +1,3 @@
+https://discuss.tensorflow.org/t/tensorflow-not-detecting-gpu/16295
+
+Starting with TensorFlow 2.11, you will need to install TensorFlow in WSL2, or install tensorflow-cpu` and, optionally, try the TensorFlow-DirectML-Plugin. For installing WSL2 please refer to this document 816. Thank You.
--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import tensorflow as tf
 
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 else:
-    print("Please install GPU version of TF")
+    print("No GPU detected.")
